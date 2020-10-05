599309e69023e6232d85fb6f86f61941bc788f51
梁峰宁 <liangfengning@foxmail.com>
2020-10-05 21:13:33 +0800

    first version

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
+const fs = require('fs');
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const OSS = require('ali-oss');
+const STS = OSS.STS;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+require('dotenv').config();
+
+const policy = fs.readFileSync('./policy.conf.json').toString();
+
+const sts = new STS({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  accessKeySecret: process.env.ACCESS_KEY_SECRET,
+});
+
+express.json();
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  try {
+    if (req.query.id) {
+      const token = await sts.assumeRole(
+        process.env.ROLE_ARN,
+        policy,
+        process.env.EXPIRATION,
+        process.env.SESSION_NAME
+      );
+
+      res.json({
+        code: 0,
+        msg: '成功获得 STS Token',
+        data: {
+          region: 'oss-cn-beijing.aliyuncs.com',
+          accessKeyId: token.credentials.AccessKeyId,
+          accessKeySecret: token.credentials.AccessKeySecret,
+          stsToken: token.credentials.SecurityToken,
+          bucket: 'lifeni-test',
+        },
+      });
+    } else {
+      res.statusCode = 400;
+      res.json({
+        code: 1,
+        msg: '没有认证',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(7070);
